0ee25b0ad8fd59d6eb3e2ee0439e950befaef467
三浦宏予 <yoooon0908@yahoo.co.jp>
2016-02-16 14:05:40 +0800

    start commit

---
+++ EnglishDiary/fourthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  fourthViewController.swift
+//  EnglishDiary
+//
+//  Created by 三浦宏予 on 2016/02/16.
+//  Copyright © 2016年 Hiroyo Miura. All rights reserved.
+//
+
+import UIKit
+
+class fourthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
